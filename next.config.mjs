05e2665786a2ac856a1e009eddf0a5f8e623d04d
Eyyan <eyyansalman49@gmail.com>
2025-11-27 12:32:09 +0500

    main

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",      // ⭐ ADD THIS LINE
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
